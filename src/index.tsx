eab9b22f263d32f8d499fbb69f0bb7024480e370
Conner Ruhl <connerruhl@me.com>
2018-02-11 16:39:12 -0600

    Using common typescript tooling, page is now showing "Go!"

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,4 @@
-// This import will be replaced with polyfill imports by babel-preset-env
-import "babel-polyfill";
-
 import * as React from "react";
 import { render } from "react-dom";
 
-import "assets/styles/main.scss";
-import StatefulClassComponent from "components/StatefulClass";
-import StatelessFunctionComponent from "components/StatelessFunction";
-
-render(
-  <div>
-    <StatelessFunctionComponent message="Hello World" />
-    <StatefulClassComponent />
-  </div>,
-  document.getElementById("app")
-);
+render(<div>Go!</div>, document.getElementById("app"));
