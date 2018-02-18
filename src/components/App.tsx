0fc21bbd29a4ec5b83c1731a12c7de7da10de6c2
Conner Ruhl <connerruhl@me.com>
2018-02-18 10:44:02 -0600

    Basic app running with `graph-computer` as the state engine

---
+++ src/components/App.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+// import { graph } from "graph-computer";
+
+export default () => <div>Go!</div>;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
 import { render } from "react-dom";
 
-render(<div>Go!</div>, document.getElementById("app"));
+import App from "./components/App";
+
+render(<App />, document.getElementById("app"));
