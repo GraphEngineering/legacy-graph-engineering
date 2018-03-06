917de05c5c4ac1dd1a733c40463e1d109c563261
Conner Ruhl <connerruhl@me.com>
2018-03-06 09:00:03 -0600

    Refactoring withGraphQL to support fragments

--- src/components/Schema.tsx
+++ src/components/Schema.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-
-import { introspectionQuery } from "graphql";
 import gql from "graphql-tag";
 
 import { GraphQLComponent, withGraphQL } from "./withGraphQL";
--- src/components/withGraphQL/index.ts
+++ src/components/withGraphQL/index.ts
@@ -1,45 +1,96 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import { DocumentNode } from "graphql";
+import { DocumentNode, DefinitionNode, OperationDefinitionNode } from "graphql";
+import { Dispatch } from "redux";
 
-import {
-  QueryProps,
-  QueryMapper,
-  mapperFromQueryDefinition
-} from "./mapperFromQueryDefinition";
+export type GraphQLComponent<Operations = {}> = React.StatelessComponent<
+  Operations
+>;
 
-import {
-  MutationProps,
-  MutationMapper,
-  mapperFromMutationDefinition
-} from "./mapperFromMutationDefinition";
+export const withGraphQL = <Operations>(
+  document: DocumentNode,
+  component: GraphQLComponent<Operations>
+) => {
+  const documentWithoutMutations = removeOperationType("mutation", document);
+  const documentWithoutQueries = removeOperationType("query", document);
 
-import definitionsFromDocument from "./definitionsFromDocument";
-
-export type GraphQLComponent<
-  Query = {},
-  Mutation = {}
-> = React.StatelessComponent<GraphQLComponentProps<Query, Mutation>>;
-
-export type GraphQLComponentProps<Query, Mutation> = QueryProps<Query> &
-  MutationProps<Mutation>;
-
-export const withGraphQL = <Query = {}, Mutation = {}>(
-  component: GraphQLComponent<Query, Mutation>
-) => (document: DocumentNode) => {
-  const { queryDefinition, mutationDefinition } = definitionsFromDocument(
-    document
-  );
-
-  const mapStateToProps =
-    queryDefinition && mapperFromQueryDefinition<Query>(queryDefinition);
-
-  const mapDispatchToProps =
-    mutationDefinition &&
-    mapperFromMutationDefinition<Mutation>(mutationDefinition);
+  const mapStateToProps = documentToStateMapper(documentWithoutMutations);
+  const mapDispatchToProps = documentToDispatchMapper(documentWithoutQueries);
 
   return connect(
-    mapStateToProps as QueryMapper<Query>,
-    mapDispatchToProps as MutationMapper<Mutation>
+    mapStateToProps as QueryMapper<Operations>,
+    mapDispatchToProps as MutationMapper<Operations>
   )(component);
 };
+
+const removeOperationType = (
+  operation: "query" | "mutation",
+  document: DocumentNode
+): DocumentNode => ({
+  ...document,
+  definitions: document.definitions.filter(
+    definition =>
+      isOperationDefinition(definition) && definition.operation !== operation
+  )
+});
+
+export type DispatchMapper = (
+  dispatch: Dispatch<any>
+) => { [mutationName: string]: () => any };
+
+const documentToDispatchMapper = (document: DocumentNode): DispatchMapper => {
+  const namedMutations = document.definitions.filter(
+    definition =>
+      isOperationDefinition(definition) &&
+      definition.operation === "mutation" &&
+      definition.name
+  );
+
+  namedMutations.reduce(
+    (dispatchers, mutation) => ({
+      ...dispatchers
+    }),
+    {}
+  );
+
+  return dispatch => ({
+    mutation: mutationDispatcher(dispatch) as Mutation
+  });
+};
+
+const isOperationDefinition = (
+  definition: DefinitionNode
+): definition is OperationDefinitionNode =>
+  definition.kind === "OperationDefinition";
+
+// export type MutationMapper<Mutation> = (
+//   dispatch: Dispatch<any>
+// ) => MutationProps<Mutation>;
+
+// export interface MutationProps<Mutation> {
+//   mutation: Mutation;
+// }
+
+// export const mapperFromMutationDefinitions = <Mutation>(
+//   definition: OperationDefinitionNode
+// ): MutationMapper<Mutation> => {
+//   const mutationDispatcher = (dispatch: Dispatch<any>) =>
+//     definition.selectionSet.selections.reduce((mutations, selection) => {
+//       if (
+//         selection.kind === "FragmentSpread" ||
+//         selection.kind === "InlineFragment"
+//       ) {
+//         return mutations;
+//       }
+
+//       const source = print(definition);
+//       return {
+//         ...mutations,
+//         [selection.name.value]: () => dispatch({ type: source })
+//       };
+//     }, {});
+
+//   return dispatch => ({
+//     mutation: mutationDispatcher(dispatch) as Mutation
+//   });
+// };
