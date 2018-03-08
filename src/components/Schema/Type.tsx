a95094553dbc890c48e5cd33fc90db1c1bc6d1b4
Conner Ruhl <connerruhl@me.com>
2018-03-07 23:29:48 -0600

    Type annotations now working

--- src/components/Schema/Type.tsx
+++ src/components/Schema/Type.tsx
@@ -4,7 +4,9 @@ import {
   IntrospectionType,
   IntrospectionObjectType,
   IntrospectionField,
-  IntrospectionNamedTypeRef
+  IntrospectionTypeRef,
+  IntrospectionListTypeRef,
+  IntrospectionNonNullTypeRef
 } from "graphql";
 
 export default (type: IntrospectionType) =>
@@ -18,7 +20,6 @@ const ObjectType = ({
 }: IntrospectionObjectType) => (
   <div id={typeId(name)} className="type object" key={name}>
     <div className="definition">
-      type
       <span className="name">{name}</span>
       {interfaces.length > 0 && (
         <span className="implements">
@@ -35,7 +36,7 @@ const ObjectType = ({
         </span>
       )}
     </div>
-    {/* {description && <span className="description">{description}<span/>} */}
+    {description && <div className="description">{description}</div>}
     <div className="fields">{fields.map(Field)}</div>
   </div>
 );
@@ -53,7 +54,47 @@ const Field = ({
   <div className="field" key={name}>
     <div className="definition">
       <span className="name">{name}</span>
+      <TypeRef type={type} />
     </div>
     {description && <div className="description">{description}</div>}
   </div>
 );
+
+const TypeRef: React.StatelessComponent<{
+  type: IntrospectionTypeRef;
+}> = ({ type }) =>
+  isTypeRefList(type) ? (
+    <span className="type-ref list">
+      {type.ofType && (
+        <React.Fragment>
+          [<TypeRef type={type.ofType} />]
+        </React.Fragment>
+      )}
+    </span>
+  ) : isTypeRefNonNull(type) ? (
+    <span className="type-ref non-null">
+      {type.ofType && (
+        <React.Fragment>
+          <TypeRef type={type.ofType} />!
+        </React.Fragment>
+      )}
+    </span>
+  ) : (
+    <span className="type-ref named">
+      {type.kind !== "SCALAR" ? (
+        <a className="name" href={`#${typeId(type.name)}`}>
+          {type.name}
+        </a>
+      ) : (
+        <span className="name">{type.name}</span>
+      )}
+    </span>
+  );
+
+const isTypeRefList = (
+  type: IntrospectionTypeRef
+): type is IntrospectionListTypeRef => type.kind === "LIST";
+
+const isTypeRefNonNull = (
+  type: IntrospectionTypeRef
+): type is IntrospectionNonNullTypeRef => type.kind === "NON_NULL";
